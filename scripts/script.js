65965603e2e4e9583758c40aeb03663cc6cf6d3c
semenova-ev-nova <elizaveta.semenova.785@gmail.com>
2025-06-27 20:42:06 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,67 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* 1. Исключение накладывания контента на хедер при скроле/прокрутке страницы */
+
+    const header = document.querySelector('.header');       // создаем переменную находя блок по классу
+        if (header) {                                           // проверяем существование элемента в DOM
+        console.log('Константа header существует');
+
+        /* 
+        *   Алгоритм
+        *
+        *   1. Начало.
+        *   2. Получаем высоту блока/элемента (создание переменной, которая не будет меняться).
+        *   3. Проверка условия (навешиваем слушатель событий на scroll страницы и ожидаем ее прокрутку): если страница прокручивается.
+        *       3.1. Да: Получаем значение насколько прокрутили страницу (создание переменной, которая будет меняться).
+        *           3.1.1 Проверка условия (сравниваем высоту элемента и значение прокрученной страницы): если расстояние от верха страницы больше высоты элемента
+        *               3.1.1.1. Да: устанавливаем класс модификатора на элемент
+        *               3.1.1.2. Нет (если расстояние от верха экрана меньше высоты элемента): удаляем класс модификатора у элемента
+        *       3.2. Нет: Конец
+        *   4. Конец
+        */
+
+        const heightHeader = header.offsetHeight;           // определяем высоту блока, включая внутренние отступы
+
+        document.addEventListener('scroll', () => {         // навешиваем слушатель событий на scroll страницы и ожидаем ее прокрутку
+
+            console.log('Страница скролится');
+
+            let scrollPageY = this.scrollY;                 // получаем значение насколько прокрутили страницу
+
+            if (scrollPageY > heightHeader) {               // условие: если расстояние от верха страницы больше высоты элемента
+                header.classList.add('header--scroll')      // устанавливаем класс модификатора на элемент
+            } else {
+                header.classList.remove('header--scroll')   // удаляем класс модификатора у элемента
+            }
+
+        })
+
+    }
+});
+
+
+  
+  const buttons = document.querySelectorAll(".lists__btn");
+  const cardsFilter = document.querySelectorAll(".lists__item");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const filter = button.getAttribute("data-filter"); 
+      cardsFilter.forEach((card) => {
+                if (filter === "all") {
+                    card.classList.remove("hidden");
+                } else {
+                    if (card.classList.contains(filter)) {
+            card.classList.remove("hidden");
+                    } else {
+            card.classList.add("hidden");
+                    }
+                }
+            });
+        });
+    });
+
+    
